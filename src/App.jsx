a0cbad2a8d0eab6cb7293403db0feb539a5b1111
Sangram <sangramraut7277@gmail.com>
2024-10-14 10:06:57 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+//import Cookies from "./components/Cookies"
+
+// import CollaborationSection from "./components/homePage/CollaborationSection"
+// import Footer from "./components/homePage/Footer"
+// import HeroSection from "./components/homePage/HeroSection"
+// import HeroSection2 from "./components/homePage/HeroSection2"
+// import Home from "./components/homePage/Home"
+// import JobList from "./components/homePage/JobList"
+// import Navbar from "./components/homePage/Navbar"
+// import TestimonialSlider from "./components/homePage/TestimonialSlider"
+//import WhyChooseUs from "./components/homePage/WhyChooseUs"
+
+//import About from "./components/About";
+
 //import Contactus from "./Components/Contactus"
 
 //import Header from './components/Joblisting/Header';
@@ -7,12 +21,28 @@ import Signin from "./components/Joblisting/Signin";
 
 function App() {
   return (
-    <div className="app">
-      {/* <Contactus/>   */}
-      {/* <Header/>   */}
-      {/* <Maincontent/>  */}
+    <>
+      {/*<Cookies/>*/}
+      {/* <Navbar/>
+     <HeroSection/>
+     <Home/>
+     <JobList/> */}
+      {/*<WhyChooseUs/>*/}
+      {/* <TestimonialSlider/> */}
+      {/* <CollaborationSection/> */}
+      {/* <HeroSection2/> */}
+      {/* <Footer/> */}
+      {/* <About /> */}
+
+      {/* <Header />
+      <Details />
+      <RelatedJobs /> */}
+      {/* <Signup /> */}
+      {/* <Header/> */}
+      {/* <Maincontent/> */}
       <Signin />
-    </div>
+      {/* <Contactus/> */}
+    </>
   );
 }
 
