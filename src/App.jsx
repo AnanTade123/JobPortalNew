facf5f113bd81cf4610ed1bb3e5395d79188a564
Chaitanya310 <chaitanyamore2001@gmail.com>
2024-10-15 20:15:03 +0530

    Comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 //import About from "./components/About";
 
+// import JobDetails from "./components/jobDetails/JobDetails";
+
 //import Contactus from "./Components/Contactus"
 
 //import Header from './components/Joblisting/Header';
 //import Maincontent from './components/Joblisting/Maincontent';
 // import Signin from './components/Joblisting/Signin';
 
-// import Details from "./components/jobDetails/Details";
-// import Header from "./components/jobDetails/Header";
-
-// import RelatedJobs from "./components/jobDetails/RelatedJobs";
 import Signup from "./components/jobDetails/Signup";
 
 function App() {
@@ -17,9 +15,7 @@ function App() {
     <>
       {/* <About /> */}
 
-      {/* <Header />
-      <Details />
-      <RelatedJobs /> */}
+      {/* <JobDetails /> */}
 
       <Signup />
     </>
