ce983ae5c3252bbe454338c6629951507fb6e0b7
Chaitanya310 <chaitanyamore2001@gmail.com>
2024-10-25 11:08:32 +0530

    Comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 //import Cookies from "./components/Cookies"
 
+import CandidateDetails from "./components/candidateDetails/CandidateDetails";
+
 // import HomePage from "./components/homePage/HomePage"
 
 //import About from "./components/About";
@@ -11,20 +13,22 @@
 //import Header from './components/Joblisting/Header';
 //import Maincontent from './components/Joblisting/Maincontent';
 
-import Signup from "./components/jobDetails/Signup";
+// import Signup from "./components/jobDetails/Signup";
 
 //import Signin from "./components/Joblisting/Signin";
 
 function App() {
   return (
     <>
+      <CandidateDetails />
+
       {/* <JobDetails /> */}
 
       {/* <HomePage/> */}
       {/* <Header />
       <Details />
       <RelatedJobs /> */}
-      <Signup />
+      {/* <Signup /> */}
       {/* <Header/> */}
       {/* <Maincontent/> */}
       {/* <Signin /> */}
