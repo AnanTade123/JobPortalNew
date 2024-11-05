f16f694338b1a5e3b66b2633f92e664ea5222369
Chaitanya310 <chaitanyamore2001@gmail.com>
2024-11-05 23:24:38 +0530

    Updated project with new images and restored components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+/* eslint-disable react/jsx-no-undef */
+//import { Routes, Route } from 'react-router-dom';
+//import Joblist from "h:/Job Portal Project/JobPortalNew/src/components/Joblisting/Joblist"
+//import Signin from "./components/Joblisting/Signin";
+//import Contactus from "../Contactus";
+//import Registration from "./components/Registration";
+// import CansHeader from "./components/Candidatelisting/CansHeader"
+// import Candlistmain from "./components/Candidatelisting/candlistmain";
+
 // import CandidateDetails from "./components/candidateDetails/CandidateDetails";
 
 // import HomePage from "./components/homePage/HomePage"
@@ -12,7 +21,19 @@
 // import Signup from "./components/jobDetails/Signup";
 
 //import Signin from "./components/Joblisting/Signin";
-import JobDetails from "./components/jobDetails/JobDetails";
+
+// import JobDetails from "./components/jobDetails/JobDetails";
+
+// import JobDetails from "./components/jobDetails/JobDetails";
+
+// import { Routes, Route } from 'react-router-dom';
+// import HomePage from './components/homePage/HomePage';
+// import JobDetails from './components/jobDetails/JobDetails';
+// import Signin from './components/Joblisting/Signin';
+// import Contactus from './components/Contactus';
+// import About from './components/About';
+// import Cookies from './components/Cookies'
+// import Joblist from './components/Joblisting/Joblist';
 
 // import { Routes, Route } from 'react-router-dom';
 // import HomePage from './components/homePage/HomePage';
@@ -26,9 +47,12 @@ import JobDetails from "./components/jobDetails/JobDetails";
 function App() {
   return (
     <>
+      {/* <Routes>
+         <Route path="/" element={<HomePage />} />  */}
+
       {/* <CandidateDetails /> */}
 
-      <JobDetails />
+      {/* <JobDetails /> */}
 
       {/* <HomePage/> */}
       {/* <Header />
@@ -50,8 +74,11 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/cookie-policy" element={<Cookies />} />
         <Route path="/joblist" element={<Joblist />} />
-       
-      </Routes> */}
+
+
+      </Routes>  */}
+
+      {/* </Routes> */}
     </>
   );
 }
